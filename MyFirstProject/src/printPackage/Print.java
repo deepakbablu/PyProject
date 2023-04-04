dfbfb8375c8793d55efe128403f11625f9934204
deepakbablu <47096784+deepakbablu@users.noreply.github.com>
2023-04-04 10:20:46 +0530

    Add files via upload

---
+++ MyFirstProject/src/printPackage/Print.java
@@ -0,0 +1,9 @@
+package printPackage;
+
+public class Print {
+	public static void main(String[] args) {
+		int a=2;
+		int b=23;
+	System.out.println("hi,Iam console based application"+ a*b);
+	}
+}
